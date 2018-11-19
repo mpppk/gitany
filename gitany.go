2fe5690a3ea16d165bdf5d3970090830ab76dd48
mpppk <niboshiporipori@gmail.com>
2018-11-19 22:37:39 +0900

    Add CreateToken

--- gitany.go
+++ gitany.go
@@ -19,3 +19,16 @@ func GetClient(ctx context.Context, serviceConfig *ServiceConfig) (Client, error
 	}
 	return nil, errors.New("unknown serviceConfig type: " + serviceConfig.Type)
 }
+
+func CreateToken(ctx context.Context, serviceConfig *ServiceConfig, username, pass string) (string, error) {
+	for _, clientGenerator := range clientGenerators {
+		if clientGenerator.GetType() == serviceConfig.Type {
+			client, err := clientGenerator.NewViaBasicAuth(ctx, serviceConfig, username, pass)
+			if err != nil {
+				return "", err
+			}
+			return client.GetAuthorizations().CreateToken(ctx)
+		}
+	}
+	return "", errors.New("token creating failed because unknown serviceConfig type is provided: " + serviceConfig.Type)
+}
