cf24743b2131169a820b5b38367bd812a95c6db3
mpppk <niboshiporipori@gmail.com>
2018-11-20 14:03:41 +0900

    Add NewDefaultServiceConfig

--- github/init.go
+++ github/init.go
@@ -4,4 +4,10 @@ import "github.com/mpppk/gitany"
 
 func init() {
 	gitany.RegisterClientGenerator(&ClientGenerator{})
+
+	gitany.RegisterDefaultServiceConfig(&gitany.ServiceConfig{
+		Host:     "github.com",
+		Type:     "github",
+		Protocol: "https",
+	})
 }
